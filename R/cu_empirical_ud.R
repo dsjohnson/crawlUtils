8eeb81744fd6d4c731fd4eea47139bbee1159ed6
Josh M. London <josh.london@noaa.gov>
2022-08-05 16:23:20 -0700

    fix issue w/ incorrect npts calculation
    the npts_in_poly internal function wasn't properly passing x and y to st_intersects. Still not sure why, but it seems to be fixed now.

--- R/cu_empirical_ud.R
+++ R/cu_empirical_ud.R
@@ -4,20 +4,20 @@
 #' @param grid a grid created from [cu_ud_grid()]
 #' @param average should an average UD be created from 1 or more pts layers
 #'
-#' @return grid with an additional column, mean_pts
+#' @return grid with an additional column, _npts_ or _mean_pts_
 #' @export
 #'
 cu_empirical_ud <- function(pts, grid, average = TRUE) {
   cell <- npts <- NULL
   npts_in_poly <- function(x, y) {
-    res <- dplyr::mutate(y, npts = lengths(sf::st_intersects(y, x)))
+    y$npts = lengths(sf::st_intersects(y, x))
+    return(y)
   }
 
   if(inherits(pts,'sf')) {
     pts <- list(pts)
   }
-  res <- purrr::map_df(pts, npts_in_poly, y = grid)
-
+  res <- purrr::map(pts, ~npts_in_poly( x = ., y = grid))
   if(average) {
     res <- res %>%
       dplyr::bind_rows() %>%
@@ -25,6 +25,7 @@ cu_empirical_ud <- function(pts, grid, average = TRUE) {
       dplyr::group_by(cell) %>%
       dplyr::summarise(mean_pts = mean(npts))
     res <- grid %>% dplyr::left_join(res, by="cell")
+    return(res)
   }
-  return(res)
+  return(pluck(res,1))
 }
